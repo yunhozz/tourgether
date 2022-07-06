f0ce59e92cfe063a89bb186ce27fa66ad2847e16
yunhozz <qkrdbsgh1121@naver.com>
2022-07-06 11:30:42 +0900

    Feat: Delete 'Httpsession'

--- src/main/java/com/tourgether/global/auth/UserDetailsServiceImpl.java
+++ src/main/java/com/tourgether/global/auth/UserDetailsServiceImpl.java
@@ -1,17 +1,11 @@
-package com.tourgether.domain.member.service;
+package com.tourgether.global.auth;
 
 import com.tourgether.domain.member.Member;
-import com.tourgether.domain.member.UserDetailsImpl;
 import com.tourgether.domain.member.repository.MemberRepository;
-import com.tourgether.global.dto.MemberSessionResponseDto;
-import com.tourgether.global.ui.SessionConstants;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.http.HttpSession;
 
 @Component
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -19,15 +13,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private MemberRepository memberRepository;
 
-    @Autowired
-    private HttpSession session;
-
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetailsImpl loadUserByUsername(String email) throws UsernameNotFoundException {
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException(email));
-        session.setAttribute(SessionConstants.LOGIN_MEMBER, new MemberSessionResponseDto(member));
-
         return new UserDetailsImpl(member);
     }
 }
