c78f5ca7257d161dc812b5d8ff154bcf4b207471
yunhozz <qkrdbsgh1121@naver.com>
2022-07-06 11:19:38 +0900

    Feat: Add 'oAuth2Id', 'role'

--- src/main/java/com/tourgether/domain/member/dto/MemberResponseDto.java
+++ src/main/java/com/tourgether/domain/member/dto/MemberResponseDto.java
@@ -1,6 +1,7 @@
 package com.tourgether.domain.member.dto;
 
 import com.tourgether.domain.member.Member;
+import com.tourgether.global.enums.Role;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,23 +12,25 @@ import java.time.LocalDateTime;
 public class MemberResponseDto {
 
     private Long id;
+    private String oAuth2Id;
     private String email;
     private String password;
     private String name;
     private String nickname;
     private String profileUrl;
-    private String auth;
+    private Role role;
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
 
     public MemberResponseDto(Member member) {
         id = member.getId();
+        oAuth2Id = member.getOAuth2Id();
         email = member.getEmail();
         password = member.getPassword();
         name = member.getName();
         nickname = member.getNickname();
         profileUrl = member.getProfileUrl();
-        auth = member.getAuth();
+        role = member.getRole();
         createdDate = member.getCreatedDate();
         lastModifiedDate = member.getLastModifiedDate();
     }
