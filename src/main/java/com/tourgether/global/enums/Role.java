4f36572c47a60e84c03c6eccf279eb4a2295929b
yunhozz <qkrdbsgh1121@naver.com>
2022-07-06 11:15:21 +0900

    Feat: Initial commit

---
+++ src/main/java/com/tourgether/global/enums/Role.java
@@ -0,0 +1,15 @@
+package com.tourgether.global.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    ADMIN("ROLE_ADMIN", "운영자"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String value;
+}
