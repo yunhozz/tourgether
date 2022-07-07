dfde825e6f7d123b5f0f4f1c31a690dc54129e14
yunhozz <qkrdbsgh1121@naver.com>
2022-07-07 17:59:08 +0900

    Fix: Change annotation
    '@EnableWebSecurity' -> '@RequiredArgsConstructor'

--- src/main/java/com/tourgether/global/config/WebSecurityConfig.java
+++ src/main/java/com/tourgether/global/config/WebSecurityConfig.java
@@ -4,14 +4,12 @@ import com.tourgether.domain.member.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
     @Bean
