6a72ead34d00b1d66dfd97a898c4d28a054c798e
yunhozz <qkrdbsgh1121@naver.com>
2022-07-07 17:59:59 +0900

    Feat: Add OAuth2 features

--- src/main/java/com/tourgether/global/config/WebSecurityConfig.java
+++ src/main/java/com/tourgether/global/config/WebSecurityConfig.java
@@ -1,21 +1,24 @@
 package com.tourgether.global.config;
 
-import com.tourgether.domain.member.service.UserDetailsServiceImpl;
+import com.tourgether.domain.member.service.OAuth2UserServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new UserDetailsServiceImpl();
-    }
+    private final OAuth2UserServiceImpl oAuth2UserService;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -23,27 +26,35 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(kakaoClientRegistration());
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf().disable()
+                .csrf().disable() // 추후 제거 예정
                 .authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .anyRequest().permitAll()
+                .anyRequest().permitAll() // 추후 수정 예정
 
                 .and()
 
                 .formLogin()
-                .loginPage("/login")
+                .loginPage("/sign-in")
                 .usernameParameter("email")
                 .passwordParameter("password")
                 .defaultSuccessUrl("/")
-
                 .and()
-
                 .logout()
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/")
-                .invalidateHttpSession(true);
+                .invalidateHttpSession(true)
+
+                .and()
+
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(oAuth2UserService);
 
         return httpSecurity.build();
     }
@@ -51,5 +62,21 @@ public class WebSecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring().antMatchers("/resources/**", "/h2-console/**");
+    }
+
+    // kakao client 등록
+    private ClientRegistration kakaoClientRegistration() {
+        return ClientRegistration.withRegistrationId("kakao")
+                .clientId("326f5c4c2ca1457d9c6c12db344b79f8")
+                .redirectUri("{baseUrl}/{action}/oauth2/code/{registrationId}")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .scope("profile_nickname", "profile_image", "account_email")
+                .clientName("kakao")
+                .authorizationUri("https://kauth.kakao.com/oauth/authorize")
+                .tokenUri("https://kauth.kakao.com/oauth/token")
+                .userInfoUri("https://kapi.kakao.com/v2/user/me")
+                .userNameAttributeName("id")
+                .build();
     }
 }
