869d38747692f643b3c1b687252af58b99dd13ac
yunhozz <qkrdbsgh1121@naver.com>
2022-07-11 18:05:43 +0900

    Feat: Initial commit

---
+++ src/main/java/com/tourgether/domain/recruit/model/entity/Bookmark.java
@@ -0,0 +1,32 @@
+package com.tourgether.domain.recruit.model.entity;
+
+import com.tourgether.domain.TimeEntity;
+import com.tourgether.domain.member.model.entity.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bookmark extends TimeEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recruit_id")
+    private Recruit recruit;
+
+    public Bookmark(Member member, Recruit recruit) {
+        this.member = member;
+        this.recruit = recruit;
+    }
+}
