65991002054fc89615baf818ed5ddb232808aeb7
yunhozz <qkrdbsgh1121@naver.com>
2022-07-14 19:01:32 +0900

    Feat: Add 'addView' query

--- src/main/java/com/tourgether/domain/recruit/model/repository/RecruitRepository.java
+++ src/main/java/com/tourgether/domain/recruit/model/repository/RecruitRepository.java
@@ -2,6 +2,13 @@ package com.tourgether.domain.recruit.model.repository;
 
 import com.tourgether.domain.recruit.model.entity.Recruit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface RecruitRepository extends JpaRepository<Recruit, Long> {
+public interface RecruitRepository extends JpaRepository<Recruit, Long>, RecruitRepositoryCustom {
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Recruit r set r.view = r.view + 1 where r.id = :id")
+    void addView(@Param("id") Long id);
 }
