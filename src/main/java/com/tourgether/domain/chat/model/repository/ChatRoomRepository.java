0066f981903f066f562d4c329ce144b61ee5725e
yunhozz <qkrdbsgh1121@naver.com>
2022-07-18 16:29:23 +0900

    Fix: To add an extension to 'ChatRoomRepositoryCustom'

--- src/main/java/com/tourgether/domain/chat/model/repository/ChatRoomRepository.java
+++ src/main/java/com/tourgether/domain/chat/model/repository/ChatRoomRepository.java
@@ -3,5 +3,5 @@ package com.tourgether.domain.chat.model.repository;
 import com.tourgether.domain.chat.model.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>, ChatRoomRepositoryCustom {
 }
