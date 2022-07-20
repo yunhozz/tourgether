de1c7e5678c1b74f5535d322cf904ad2d504dbd7
yunhozz <qkrdbsgh1121@naver.com>
2022-07-20 18:56:41 +0900

    Feat: Add 'deleteRecruit' method in order to break off the relationship

--- src/main/java/com/tourgether/domain/recruit/model/entity/Bookmark.java
+++ src/main/java/com/tourgether/domain/recruit/model/entity/Bookmark.java
@@ -29,4 +29,8 @@ public class Bookmark extends TimeEntity {
         this.member = member;
         this.recruit = recruit;
     }
+
+    public void deleteRecruit() {
+        recruit = null;
+    }
 }
