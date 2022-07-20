6d1f16e9c716cbd8e55a157e19307557c7264881
yunhozz <qkrdbsgh1121@naver.com>
2022-07-20 18:57:18 +0900

    Feat: Add queries
    Add 'findWithRecruitId' and 'deleteBookmark'

--- src/main/java/com/tourgether/domain/recruit/model/repository/BookmarkRepository.java
+++ src/main/java/com/tourgether/domain/recruit/model/repository/BookmarkRepository.java
@@ -2,6 +2,18 @@ package com.tourgether.domain.recruit.model.repository;
 
 import com.tourgether.domain.recruit.model.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    @Query("select b from Bookmark b join fetch b.recruit r where r.id = :recruitId")
+    List<Bookmark> findWithRecruitId(@Param("recruitId") Long recruitId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete from Bookmark b where b.id = :id")
+    void deleteBookmark(@Param("id") Long id);
 }
