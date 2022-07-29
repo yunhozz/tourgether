aa19623e8729e4da19a291774a5d51fcaad19203
yunhozz <qkrdbsgh1121@naver.com>
2022-07-29 16:56:33 +0900

    Feat: Add teamId column

--- src/main/java/com/tourgether/domain/member/model/dto/response/MemberResponseDto.java
+++ src/main/java/com/tourgether/domain/member/model/dto/response/MemberResponseDto.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 public class MemberResponseDto {
 
     private Long id;
+    private Long teamId;
     private String oAuth2Id;
     private String email;
     private String password;
@@ -24,6 +25,7 @@ public class MemberResponseDto {
 
     public MemberResponseDto(Member member) {
         id = member.getId();
+        teamId = member.getTeam().getId();
         oAuth2Id = member.getOAuth2Id();
         email = member.getEmail();
         password = member.getPassword();
