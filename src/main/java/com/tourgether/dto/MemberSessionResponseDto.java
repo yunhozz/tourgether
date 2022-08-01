c4127c4b4a4356f07fd8ee8eaa285d3bf759a193
yunhozz <qkrdbsgh1121@naver.com>
2022-08-01 13:28:08 +0900

    Fix: Add optional on 'team.getId()' because of NPE

--- src/main/java/com/tourgether/dto/MemberSessionResponseDto.java
+++ src/main/java/com/tourgether/dto/MemberSessionResponseDto.java
@@ -1,35 +1,35 @@
 package com.tourgether.dto;
 
 import com.tourgether.domain.member.model.entity.Member;
-import com.tourgether.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Getter
 @AllArgsConstructor
 public class MemberSessionResponseDto implements Serializable {
 
     private Long id;
+    private Long teamId;
     private String email;
     private String password;
     private String name;
     private String nickname;
     private String profileImgUrl;
-    private Role role;
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
 
     public MemberSessionResponseDto(Member member) {
         id = member.getId();
+        Optional.ofNullable(member.getTeam()).ifPresent(team -> teamId = team.getId());
         email = member.getEmail();
         password = member.getPassword();
         name = member.getName();
         nickname = member.getNickname();
         profileImgUrl = member.getProfileImgUrl();
-        role = member.getRole();
         createdDate = member.getCreatedDate();
         lastModifiedDate = member.getLastModifiedDate();
     }
