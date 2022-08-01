074aada9207394db77ea55bfa3dae4beaabfcd23
yunhozz <qkrdbsgh1121@naver.com>
2022-08-01 13:25:58 +0900

    Feat: Add 'findMembers', 'signup' and 'login' method for REST api

--- src/main/java/com/tourgether/api/MemberApiController.java
+++ src/main/java/com/tourgether/api/MemberApiController.java
@@ -1,24 +1,45 @@
 package com.tourgether.api;
 
-import com.tourgether.domain.member.service.OAuth2UserServiceImpl;
+import com.tourgether.domain.member.controller.form.LoginForm;
+import com.tourgether.domain.member.model.dto.request.MemberRequestDto;
+import com.tourgether.domain.member.model.dto.response.MemberResponseDto;
+import com.tourgether.domain.member.service.MemberService;
+import com.tourgether.domain.member.service.UserDetailsServiceImpl;
+import com.tourgether.ui.SessionConstants;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class MemberApiController {
 
-    private final OAuth2UserServiceImpl oAuth2UserService;
+    private final MemberService memberService;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final HttpSession session;
 
-    @GetMapping("/kakao")
-    public void kakaoCallback(@RequestParam String code) throws IOException {
-        String accessToken = oAuth2UserService.getKakaoAccessToken(code);
-        oAuth2UserService.createKakaoUser(accessToken);
+    @GetMapping("/members")
+    public List<MemberResponseDto> findMembers() {
+        return memberService.findMemberDtoList();
+    }
+
+    @GetMapping("/member/signup")
+    public ResponseEntity<MemberResponseDto> signup(@Valid MemberRequestDto memberRequestDto) {
+        Long memberId = memberService.join(memberRequestDto);
+        return ResponseEntity.ok(memberService.findMemberDto(memberId));
+    }
+
+    @PostMapping("/member/login")
+    public ResponseEntity<Object> login(@Valid LoginForm loginForm) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(loginForm.getEmail());
+        memberService.login(userDetails, loginForm.getPassword());
+
+        return ResponseEntity.ok(session.getAttribute(SessionConstants.LOGIN_MEMBER));
     }
 }
