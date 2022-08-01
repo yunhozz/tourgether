61f6af1aad23c5dd6ef2dc5ec378f45825d41b8a
yunhozz <qkrdbsgh1121@naver.com>
2022-08-01 13:21:33 +0900

    Feat: Add authorization save feature on 'initAuthority' method

--- src/main/java/com/tourgether/ui/InitDb.java
+++ src/main/java/com/tourgether/ui/InitDb.java
@@ -1,11 +1,15 @@
 package com.tourgether.ui;
 
+import com.tourgether.domain.member.model.entity.Authority;
 import com.tourgether.domain.member.model.entity.Member;
-import com.tourgether.enums.Role;
+import com.tourgether.domain.member.model.entity.MemberAuthority;
+import com.tourgether.domain.member.model.repository.AuthorityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 
 @Component
@@ -14,7 +18,9 @@ public class InitDb {
 
     private final InitService initService;
 
+    @PostConstruct
     public void init() throws Exception {
+        initService.initAuthority();
         initService.initMember();
     }
 
@@ -24,6 +30,13 @@ public class InitDb {
     public static class InitService {
 
         private final EntityManager em;
+
+        public void initAuthority() {
+            Authority auth1 = new Authority("ROLE_ADMIN");
+            Authority auth2 = new Authority("ROLE_USER");
+            em.persist(auth1);
+            em.persist(auth2);
+        }
 
         public void initMember() throws Exception {
             for (int i = 1; i <= 5; i++) {
