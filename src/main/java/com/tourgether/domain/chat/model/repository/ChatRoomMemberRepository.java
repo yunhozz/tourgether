6da19e6732ca82086f38fa60482e3ddc817becea
yunhozz <qkrdbsgh1121@naver.com>
2022-08-04 22:49:06 +0900

    Remove: Delete file

--- src/main/java/com/tourgether/domain/chat/model/repository/ChatRoomMemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tourgether.domain.chat.model.repository;
-
-import com.tourgether.domain.chat.model.entity.ChatRoomMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomMemberRepository extends JpaRepository<ChatRoomMember, Long> {
-}
