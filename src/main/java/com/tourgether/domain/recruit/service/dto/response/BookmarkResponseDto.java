a4001b3a7001f6d3051af16483168f0f5080bef5
yunhozz <qkrdbsgh1121@naver.com>
2022-08-05 21:04:17 +0900

    Feat: Add createdDate, lastModifiedDate

--- src/main/java/com/tourgether/domain/recruit/service/dto/response/BookmarkResponseDto.java
+++ src/main/java/com/tourgether/domain/recruit/service/dto/response/BookmarkResponseDto.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,10 +15,14 @@ public class BookmarkResponseDto {
     private Long id;
     private Long userId;
     private Long recruitId;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
 
     public BookmarkResponseDto(Bookmark bookmark) {
         id = bookmark.getId();
         userId = bookmark.getMember().getId();
         recruitId = bookmark.getRecruit().getId();
+        createdDate = bookmark.getCreatedDate();
+        lastModifiedDate = bookmark.getLastModifiedDate();
     }
 }
