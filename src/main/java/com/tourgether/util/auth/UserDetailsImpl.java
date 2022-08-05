fc3a0e3d257cb8c01fa423f0f189bbad02b92cbd
yunhozz <qkrdbsgh1121@naver.com>
2022-08-05 18:41:32 +0900

    Fix: Change to save column values instead of saving member entities

--- src/main/java/com/tourgether/util/auth/UserDetailsImpl.java
+++ src/main/java/com/tourgether/util/auth/UserDetailsImpl.java
@@ -1,8 +1,9 @@
 package com.tourgether.util.auth;
 
 import com.tourgether.domain.member.model.entity.Member;
+import com.tourgether.domain.member.model.entity.auth.MemberAuthority;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,15 +14,32 @@ import java.util.List;
 import java.util.Set;
 
 @Getter
-@RequiredArgsConstructor
 public class UserDetailsImpl implements UserDetails {
 
-    private final Member member;
+    private final Long id;
+    private final Set<MemberAuthority> authorities;
+    private final String oauth2Id;
+    private final String email;
+    private final String password;
+    private final String name;
+    private final String nickname;
+    private final String profileImgUrl;
+
+    public UserDetailsImpl(Member member) {
+        id = member.getId();
+        authorities = member.getAuthorities();
+        oauth2Id = member.getOAuth2Id();
+        email = member.getEmail();
+        password = member.getPassword();
+        name = member.getName();
+        nickname = member.getNickname();
+        profileImgUrl = member.getProfileImgUrl();
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Set<SimpleGrantedAuthority> authorities = new HashSet<>();
-        List<String> roles = member.getAuthorities().stream()
+        List<String> roles = this.authorities.stream()
                 .map(memberAuthority -> memberAuthority.getAuthority().getAuthorityName())
                 .toList();
 
@@ -33,12 +51,12 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public String getUsername() {
-        return member.getEmail();
+        return email;
     }
 
     @Override
     public String getPassword() {
-        return member.getPassword();
+        return password;
     }
 
     @Override
