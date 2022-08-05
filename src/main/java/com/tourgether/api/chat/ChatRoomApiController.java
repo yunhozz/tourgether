051399ec042e3fbbd26c4fca06a41d7835a30bb5
yunhozz <qkrdbsgh1121@naver.com>
2022-08-06 06:39:21 +0900

    Feat: Initial commit

---
+++ src/main/java/com/tourgether/api/chat/ChatRoomApiController.java
@@ -0,0 +1,45 @@
+package com.tourgether.api.chat;
+
+import com.tourgether.domain.chat.service.ChatRoomService;
+import com.tourgether.domain.chat.dto.request.ChatRoomRequestDto;
+import com.tourgether.domain.chat.dto.response.ChatRoomResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ChatRoomApiController {
+
+    private final ChatRoomService chatRoomService;
+
+    @GetMapping("/chatroom/{chatRoomId}")
+    public ChatRoomResponseDto getChatRoom(@PathVariable String chatRoomId) {
+        return chatRoomService.findChatRoomDto(Long.valueOf(chatRoomId));
+    }
+
+    @GetMapping("/chatroom/list")
+    public List<ChatRoomResponseDto> getChatRoomList() {
+        return chatRoomService.findChatRoomDtoList();
+    }
+
+    @PostMapping("/chatroom/create")
+    public ResponseEntity<Long> createChatRoom(@RequestBody ChatRoomRequestDto chatRoomRequestDto, @RequestParam String userId) {
+        return ResponseEntity.ok(chatRoomService.makeChatRoom(chatRoomRequestDto, Long.valueOf(userId)));
+    }
+
+    @PatchMapping("/chatroom/{chatRoomId}/update")
+    public String updateChatRoom(@PathVariable String chatRoomId, @RequestParam String title) {
+        chatRoomService.updateTitle(Long.valueOf(chatRoomId), title);
+        return "update complete";
+    }
+
+    @DeleteMapping("/chatroom/{chatRoomId}/delete")
+    public String deleteChatRoom(@PathVariable String chatRoomId) {
+        chatRoomService.deleteChatRoom(Long.valueOf(chatRoomId));
+        return "chat room(id : " + chatRoomId + ") is deleted.";
+    }
+}
---
+++ src/main/java/com/tourgether/domain/chat/model/entity/Chat.java
@@ -0,0 +1,43 @@
+package com.tourgether.domain.chat.model.entity;
+
+import com.tourgether.domain.TimeEntity;
+import com.tourgether.domain.member.model.entity.Member;
+import com.tourgether.enums.MessageType;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Chat extends TimeEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private Member sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    @Column(length = 500)
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    private MessageType type; // ENTER, TALK, EXIT
+
+    @Builder
+    private Chat(Member sender, ChatRoom chatRoom, String message, MessageType type) {
+        this.sender = sender;
+        this.chatRoom = chatRoom;
+        this.message = message;
+        this.type = type;
+    }
+}
