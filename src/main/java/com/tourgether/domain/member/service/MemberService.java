466dc2d7b681877eb6677e92a1675e74bd3a02f2
yunhozz <qkrdbsgh1121@naver.com>
2022-08-11 14:37:03 +0900

    feat: Adding token generation and session registration to the login method

--- src/main/java/com/tourgether/domain/member/service/MemberService.java
+++ src/main/java/com/tourgether/domain/member/service/MemberService.java
@@ -3,22 +3,27 @@ package com.tourgether.domain.member.service;
 import com.tourgether.domain.member.model.entity.auth.Authority;
 import com.tourgether.domain.member.model.entity.Member;
 import com.tourgether.domain.member.model.entity.auth.MemberAuthority;
-import com.tourgether.domain.member.model.repository.AuthorityRepository;
-import com.tourgether.domain.member.model.repository.MemberAuthorityRepository;
+import com.tourgether.domain.member.model.repository.auth.AuthorityRepository;
+import com.tourgether.domain.member.model.repository.auth.MemberAuthorityRepository;
 import com.tourgether.domain.member.model.repository.MemberRepository;
 import com.tourgether.enums.ErrorCode;
 import com.tourgether.exception.member.*;
+import com.tourgether.util.auth.UserDetailsImpl;
+import com.tourgether.util.auth.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.tourgether.dto.MemberDto.*;
+import static com.tourgether.dto.TokenDto.*;
 
 @Service
 @Transactional
@@ -28,8 +33,10 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final AuthorityRepository authorityRepository;
     private final MemberAuthorityRepository memberAuthorityRepository;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtProvider jwtProvider;
     private final BCryptPasswordEncoder encoder;
-    private final HttpSession session;
 
     public Long join(MemberRequestDto memberRequestDto) {
         memberRepository.findAll().forEach(
@@ -51,10 +58,17 @@ public class MemberService {
     }
 
     @Transactional(readOnly = true)
-    public void login(UserDetails userDetails, String password) {
+    public TokenResponseDto login(String email, String password) {
+        UserDetailsImpl userDetails = userDetailsService.loadUserByUsername(email);
         if (!encoder.matches(password, userDetails.getPassword())) {
-            throw new PasswordMismatchException("비밀번호가 다릅니다.", ErrorCode.PASSWORD_MISMATCH);
+            throw new PasswordMismatchException("Password is not match.", ErrorCode.PASSWORD_MISMATCH);
         }
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password); // ID, PW 를 이용한 토큰 생성
+        // 세션 등록
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return jwtProvider.createTokenDto(userDetails.getId(), authentication);
     }
 
     public void updatePassword(Long id, String originalPw, String newPw) {
