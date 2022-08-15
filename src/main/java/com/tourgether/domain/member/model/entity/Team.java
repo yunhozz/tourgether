581dd919cc862bd81095de54a6de0013581468eb
yunhozz <qkrdbsgh1121@naver.com>
2022-08-15 19:06:53 +0900

    Feat: Add constructor by static method

--- src/main/java/com/tourgether/domain/member/model/entity/Team.java
+++ src/main/java/com/tourgether/domain/member/model/entity/Team.java
@@ -22,9 +22,18 @@ public class Team extends TimeEntity {
     @Column(length = 1000)
     private String introduction;
 
-    public Team(String name, String introduction) {
+    private String propensities; // "propensityA,propensityB,propensityC ..."
+
+    private Team(String name, String introduction) {
         this.name = name;
         this.introduction = introduction;
+    }
+
+    public static Team createTeam(String name, String introduction, String... propensities) {
+        Team team = new Team(name, introduction);
+        team.updatePropensity(propensities);
+
+        return team;
     }
 
     // 정보 변경
