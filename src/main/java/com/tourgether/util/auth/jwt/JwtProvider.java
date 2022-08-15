31e5f41f7691e6ea387b9b5dcc641e9c385d3522
yunhozz <qkrdbsgh1121@naver.com>
2022-08-15 19:00:08 +0900

    Fix: Delete importation

--- src/main/java/com/tourgether/util/auth/jwt/JwtProvider.java
+++ src/main/java/com/tourgether/util/auth/jwt/JwtProvider.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
