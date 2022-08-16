d0ae8bc41b6f0f3c6fca4d4c45752561554ef48d
yunhozz <qkrdbsgh1121@naver.com>
2022-08-16 19:34:09 +0900

    Fix: Delete unnecessary variables

--- src/main/java/com/tourgether/dto/CommentDto.java
+++ src/main/java/com/tourgether/dto/CommentDto.java
@@ -1,14 +1,11 @@
 package com.tourgether.dto;
 
-import com.tourgether.domain.member.model.entity.Member;
 import com.tourgether.domain.recruit.model.entity.Comment;
-import com.tourgether.domain.recruit.model.entity.Recruit;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 public class CommentDto {
 
@@ -17,24 +14,8 @@ public class CommentDto {
     @AllArgsConstructor
     public static class CommentRequestDto {
 
-        @NotNull
-        private Long recruitId;
-
-        @NotNull
-        private Long writerId;
-
-        private Long parentId;
-
         @NotBlank
         private String content;
-
-        public Comment parentToEntity(Recruit recruit, Member writer) {
-            return Comment.createComment(recruit, writer, content);
-        }
-
-        public Comment childToEntity(Recruit recruit, Member writer, Comment parent) {
-            return Comment.createCommentChild(recruit, writer, parent, content);
-        }
     }
 
     @Getter
@@ -60,21 +41,6 @@ public class CommentDto {
     public static class UpdateForm {
 
         @NotBlank
-        private String recruitId;
-
-        @NotBlank
-        private String commentId;
-
-        @NotNull
-        private Long writerId;
-
-        @NotBlank
         private String content;
-
-        public UpdateForm(String recruitId, String commentId, Long writerId) {
-            this.recruitId = recruitId;
-            this.commentId = commentId;
-            this.writerId = writerId;
-        }
     }
 }
