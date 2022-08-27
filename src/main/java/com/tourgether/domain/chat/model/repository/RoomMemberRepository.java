966ce61ea174b68a2d016a123226ba195275a1f3
yunhozz <qkrdbsgh1121@naver.com>
2022-08-27 18:15:00 +0900

    Remove: 파일 삭제

--- src/main/java/com/tourgether/domain/chat/model/repository/RoomMemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tourgether.domain.chat.model.repository;
-
-import com.tourgether.domain.chat.model.entity.RoomMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomMemberRepository extends JpaRepository<RoomMember, Long> {
-}
