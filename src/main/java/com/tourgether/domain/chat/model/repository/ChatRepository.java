165da92e0d37d8e61a64f031827bad0e4cae886e
yunhozz <qkrdbsgh1121@naver.com>
2022-08-27 18:14:53 +0900

    Remove: 파일 삭제

--- src/main/java/com/tourgether/domain/chat/model/repository/ChatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tourgether.domain.chat.model.repository;
-
-import com.tourgether.domain.chat.model.entity.Chat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRepository extends JpaRepository<Chat, Long> {
-}
--- src/main/java/com/tourgether/domain/chat/model/repository/ChatRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tourgether.domain.chat.model.repository;
-
-import com.tourgether.domain.chat.model.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-}
