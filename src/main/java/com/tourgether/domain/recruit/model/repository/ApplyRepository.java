da55cdb40157da25305ff5c27510d8733da3257e
yunhozz <qkrdbsgh1121@naver.com>
2022-08-27 15:27:40 +0900

    Feat: 'findByMember' 쿼리 추가

--- src/main/java/com/tourgether/domain/recruit/model/repository/ApplyRepository.java
+++ src/main/java/com/tourgether/domain/recruit/model/repository/ApplyRepository.java
@@ -1,7 +1,12 @@
 package com.tourgether.domain.recruit.model.repository;
 
+import com.tourgether.domain.member.model.entity.Member;
 import com.tourgether.domain.recruit.model.entity.Apply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
+
+    List<Apply> findByMember(Member member);
 }
