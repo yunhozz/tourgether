1a94acac9479966252085c66869588d3b0877007
yunhozz <qkrdbsgh1121@naver.com>
2022-08-28 17:01:31 +0900

    Remove: 파일 삭제

--- src/main/java/com/tourgether/domain/member/model/repository/TeamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tourgether.domain.member.model.repository;
-
-import com.tourgether.domain.member.model.entity.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team, Long> {
-}
