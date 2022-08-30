6ce06bef9a0985007a110cdcdc4af1928b59c371
yunhozz <qkrdbsgh1121@naver.com>
2022-08-30 18:12:36 +0900

    Fix: 로직 수정, uri 수정

--- src/main/java/com/tourgether/api/RecruitApiController.java
+++ src/main/java/com/tourgether/api/RecruitApiController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -27,48 +28,52 @@ public class RecruitApiController {
     private final RecruitService recruitService;
     private final RecruitRepository recruitRepository;
 
-    @GetMapping("/recruits/{recruitId}")
+    @GetMapping("/recruit/{recruitId}")
     public ResponseEntity<RecruitResponseDto> getRecruit(@PathVariable String recruitId) {
         return ResponseEntity.ok(recruitService.findRecruitDto(Long.valueOf(recruitId)));
     }
 
-    @GetMapping("/recruits")
+    @GetMapping("/recruit")
     public ResponseEntity<List<RecruitResponseDto>> getRecruits() {
         return ResponseEntity.ok(recruitService.findRecruitDtoList());
     }
 
-    @GetMapping("/recruits/page")
-    public ResponseEntity<Page<RecruitQueryDto>> getRecruitsPage(@RequestParam(defaultValue = "LATEST_ORDER") SearchCondition condition, Pageable pageable) {
-        if (condition.equals(MODIFIED_ORDER)) {
+    @GetMapping("/recruit/page")
+    public ResponseEntity<Page<RecruitQueryDto>> getRecruitsPage(@RequestParam(defaultValue = "LATEST", required = false) SearchCondition condition, Pageable pageable) {
+        if (condition.equals(MODIFIED)) {
             return ResponseEntity.ok(recruitRepository.findPageWithModified(pageable));
         }
-        if (condition.equals(POPULARITY_ORDER)) {
+        if (condition.equals(POPULARITY)) {
             return ResponseEntity.ok(recruitRepository.findPageWithPopularity(pageable));
         }
         return ResponseEntity.ok(recruitRepository.findPageWithCreated(pageable));
     }
 
-    @GetMapping("/recruits/search")
-    public ResponseEntity<Object> getPageWithKeyword(@RequestParam(required = false) String keyword, @RequestParam(defaultValue = "LATEST_ORDER") SearchCondition condition, Pageable pageable) {
-        if (keyword.isEmpty()) {
+    @GetMapping("/recruit/search")
+    public ResponseEntity<Object> getPageWithKeyword(@RequestParam(required = false) String keyword, @RequestParam(defaultValue = "LATEST", required = false) SearchCondition condition, Pageable pageable) {
+        if (!StringUtils.hasText(keyword)) {
             return ResponseEntity.badRequest().body(new ErrorResponseDto(ErrorCode.KEYWORD_NOT_ENTERED));
         }
-        if (condition.equals(ACCURACY_ORDER)) {
+        if (condition.equals(ACCURACY)) {
             return ResponseEntity.ok(recruitRepository.findPageWithKeywordOnAccuracyOrder(keyword, pageable));
         }
         return ResponseEntity.ok(recruitRepository.findPageWithKeywordOnLatestOrder(keyword, pageable));
     }
 
-    @PostMapping("/recruits")
+    @PostMapping("/recruit")
     public ResponseEntity<Long> createRecruit(@AuthenticationPrincipal UserDetailsImpl userDetails, @Valid @RequestBody RecruitRequestDto recruitRequestDto) {
         return ResponseEntity.ok(recruitService.makeRecruit(userDetails.getMember().getId(), recruitRequestDto));
     }
 
-    @PatchMapping("/recruits")
+    @PatchMapping("/recruit")
     public ResponseEntity<Void> updateRecruit(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam String recruitId, @Valid @RequestBody UpdateForm updateForm) {
-        RecruitResponseDto recruit = recruitService.findRecruitDto(Long.valueOf(recruitId));
-        recruitService.updateRecruit(Long.valueOf(recruitId), recruit.getWriterId(), updateForm.getTitle(), updateForm.getContent());
-
+        recruitService.updateRecruit(Long.valueOf(recruitId), userDetails.getMember().getId(), updateForm.getTitle(), updateForm.getContent());
         return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/recruit")
+    public ResponseEntity<Void> deleteRecruit(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam String recruitId) {
+        recruitService.deleteRecruit(Long.valueOf(recruitId), userDetails.getMember().getId());
+        return ResponseEntity.noContent().build();
     }
 }
