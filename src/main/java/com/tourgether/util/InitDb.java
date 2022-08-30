3edc0dd9c6b31d912c7bf492165242e219738c8c
yunhozz <qkrdbsgh1121@naver.com>
2022-08-30 15:53:19 +0900

    Feat: 로그인, 채팅방 생성 서비스 추가

--- src/main/java/com/tourgether/util/InitDb.java
+++ src/main/java/com/tourgether/util/InitDb.java
@@ -1,17 +1,25 @@
 package com.tourgether.util;
 
+import com.tourgether.domain.chat.model.entity.Chatroom;
+import com.tourgether.domain.chat.model.repository.ChatroomRepository;
 import com.tourgether.domain.member.model.entity.auth.Authority;
 import com.tourgether.domain.member.model.entity.Member;
 import com.tourgether.domain.member.model.entity.auth.MemberAuthority;
-import com.tourgether.domain.member.model.repository.AuthorityRepository;
+import com.tourgether.domain.member.model.repository.MemberRepository;
+import com.tourgether.domain.member.model.repository.auth.AuthorityRepository;
+import com.tourgether.domain.member.model.repository.auth.MemberAuthorityRepository;
+import com.tourgether.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
-import javax.persistence.EntityManager;
 
+import static com.tourgether.dto.TokenDto.*;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class InitDb {
@@ -22,6 +30,8 @@ public class InitDb {
     public void init() throws Exception {
         initService.initAuthority();
         initService.initMember();
+        initService.initLogin();
+        initService.initChatroom();
     }
 
     @Component
@@ -29,15 +39,18 @@ public class InitDb {
     @RequiredArgsConstructor
     public static class InitService {
 
+        private final MemberService memberService;
+        private final MemberRepository memberRepository;
         private final AuthorityRepository authorityRepository;
+        private final MemberAuthorityRepository memberAuthorityRepository;
+        private final ChatroomRepository chatroomRepository;
         private final BCryptPasswordEncoder encoder;
-        private final EntityManager em;
 
         public void initAuthority() {
             Authority auth1 = new Authority("ROLE_ADMIN");
             Authority auth2 = new Authority("ROLE_USER");
-            em.persist(auth1);
-            em.persist(auth2);
+            authorityRepository.save(auth1);
+            authorityRepository.save(auth2);
         }
 
         public void initMember() throws Exception {
@@ -56,9 +69,27 @@ public class InitDb {
                 MemberAuthority memberAuthority1 = new MemberAuthority(member, roleAdmin);
                 MemberAuthority memberAuthority2 = new MemberAuthority(member, roleUser);
 
-                em.persist(member);
-                em.persist(memberAuthority1);
-                em.persist(memberAuthority2);
+                memberRepository.save(member);
+                memberAuthorityRepository.save(memberAuthority1);
+                memberAuthorityRepository.save(memberAuthority2);
+                Thread.sleep(10);
+            }
+        }
+
+        public void initLogin() throws Exception {
+            for (int i = 1; i <= 5; i++) {
+                Member member = memberRepository.getReferenceById((long) i * 3 - 2);
+                TokenResponseDto token = memberService.login(member.getEmail(), "pw" + i);
+                log.info("token" + i + " = " + token.getAccessToken());
+                Thread.sleep(10);
+            }
+        }
+
+        public void initChatroom() throws Exception {
+            for (int i = 1; i <= 5; i++) {
+                Member member = memberRepository.getReferenceById((long) i * 3 - 2);
+                Chatroom chatroom = new Chatroom(member, "title" + i);
+                chatroomRepository.save(chatroom);
                 Thread.sleep(10);
             }
         }
