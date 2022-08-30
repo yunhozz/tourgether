f48d31ed97649f27783ce3a83b3b9a6748c20e71
yunhozz <qkrdbsgh1121@naver.com>
2022-08-30 18:11:37 +0900

    Feat: 'findByRecruit' 메소드 추가

--- src/main/java/com/tourgether/domain/recruit/model/repository/CommentRepository.java
+++ src/main/java/com/tourgether/domain/recruit/model/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package com.tourgether.domain.recruit.model.repository;
 
 import com.tourgether.domain.recruit.model.entity.Comment;
+import com.tourgether.domain.recruit.model.entity.Recruit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByRecruit(Recruit recruit);
 }
