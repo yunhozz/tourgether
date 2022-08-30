f37b8c2126b917deca2c8b1f1abf018bffc52371
yunhozz <qkrdbsgh1121@naver.com>
2022-08-30 18:11:23 +0900

    Fix: 생성, 수정 시간 필드 추가

--- src/main/java/com/tourgether/dto/CommentDto.java
+++ src/main/java/com/tourgether/dto/CommentDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 public class CommentDto {
 
@@ -26,18 +27,25 @@ public class CommentDto {
         private Long id;
         private Long recruitId;
         private Long writerId;
+        private Long parentId;
         private String content;
+        private LocalDateTime createdDate;
+        private LocalDateTime lastModifiedDate;
 
         public CommentResponseDto(Comment comment) {
             id = comment.getId();
             recruitId = comment.getRecruit().getId();
             writerId = comment.getWriter().getId();
             content = comment.getContent();
+            parentId = comment.getParent() != null ? comment.getParent().getId() : null;
+            createdDate = comment.getCreatedDate();
+            lastModifiedDate = comment.getLastModifiedDate();
         }
     }
 
     @Getter
     @NoArgsConstructor
+    @AllArgsConstructor
     public static class UpdateForm {
 
         @NotBlank(message = "내용을 입력해주세요.")
