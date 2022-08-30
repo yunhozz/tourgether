d76f9c3f834aca0318794dbbec3f638ad59607f8
yunhozz <qkrdbsgh1121@naver.com>
2022-08-30 15:48:04 +0900

    Feat: 'findByRecruit' 추가

--- src/main/java/com/tourgether/domain/recruit/model/repository/ApplyRepository.java
+++ src/main/java/com/tourgether/domain/recruit/model/repository/ApplyRepository.java
@@ -2,6 +2,7 @@ package com.tourgether.domain.recruit.model.repository;
 
 import com.tourgether.domain.member.model.entity.Member;
 import com.tourgether.domain.recruit.model.entity.Apply;
+import com.tourgether.domain.recruit.model.entity.Recruit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.List;
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
 
     List<Apply> findByMember(Member member);
+    List<Apply> findByRecruit(Recruit recruit);
 }
--- src/main/java/com/tourgether/domain/recruit/model/repository/BookmarkRepository.java
+++ src/main/java/com/tourgether/domain/recruit/model/repository/BookmarkRepository.java
@@ -1,6 +1,7 @@
 package com.tourgether.domain.recruit.model.repository;
 
 import com.tourgether.domain.recruit.model.entity.Bookmark;
+import com.tourgether.domain.recruit.model.entity.Recruit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,8 +11,7 @@ import java.util.List;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long>, BookmarkRepositoryCustom {
 
-    @Query("select b from Bookmark b join fetch b.recruit r where r.id = :recruitId")
-    List<Bookmark> findWithRecruitId(@Param("recruitId") Long recruitId);
+    List<Bookmark> findByRecruit(Recruit recruit);
 
     @Modifying(clearAutomatically = true)
     @Query("delete from Bookmark b where b.id = :id")
