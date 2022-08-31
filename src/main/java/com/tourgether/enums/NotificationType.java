b58b3f6d5e1ca62e907b7fcc3b5ed9828a00b3c8
yunhozz <qkrdbsgh1121@naver.com>
2022-08-31 17:30:41 +0900

    Feat: '제안 답장' 추가

--- src/main/java/com/tourgether/enums/NotificationType.java
+++ src/main/java/com/tourgether/enums/NotificationType.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum NotificationType {
 
     OFFER("TYPE_OFFER", "참가 제안 알림"),
+    RESPONSE("TYPE_RESPONSE", "제안 답장"),
     CHAT("TYPE_CHAT", "채팅 알림"),
     BOOKMARK("TYPE_BOOKMARK", "북마크 알림"),
     COMMENT("TYPE_COMMENT", "댓글 알림");
