1be088b11e320b0f04579b5d70ea0fcb6f95d39a
yunhozz <qkrdbsgh1121@naver.com>
2022-08-31 17:30:17 +0900

    Fix: 쿼리 수정

--- src/main/java/com/tourgether/domain/notification/model/repository/NotificationRepository.java
+++ src/main/java/com/tourgether/domain/notification/model/repository/NotificationRepository.java
@@ -1,5 +1,6 @@
 package com.tourgether.domain.notification.model.repository;
 
+import com.tourgether.domain.member.model.entity.Member;
 import com.tourgether.domain.notification.model.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -10,15 +11,10 @@ import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>, NotificationRepositoryCustom {
 
-    @Query("select n from Notification n join fetch n.receiver r where r.id = :receiverId order by n.createdDate desc")
-    List<Notification> findWithReceiverId(@Param("receiverId") Long receiverId);
+    List<Notification> findByReceiverOrderByCreatedDateDesc(Member receiver);
 
     @Query("select n from Notification n join fetch n.receiver r where r.id = :receiverId and n.isChecked = :check order by n.createdDate desc")
     List<Notification> findWithReceiverIdReadOrNot(@Param("receiverId") Long receiverId, @Param("check") boolean check);
-
-    @Modifying(clearAutomatically = true)
-    @Query("delete from Notification n where n.id = :id")
-    void deleteNotification(@Param("id") Long id);
 
     @Modifying(clearAutomatically = true)
     @Query("delete from Notification n where n.id in :ids and n.isChecked = true")
